4313b11aae500fab65f7a0c65efce06a219dedda
Hemet <wilyimsyoo@gmail.com>
2023-05-02 23:39:37 -0700

    first commit

---
+++ movie_theater_homework2.sql
@@ -0,0 +1,40 @@
+INSERT INTO customer(
+	members_card,
+	e_mail,
+	first_name
+	
+) VALUES(
+	'2',
+	'mama@gmail.mia',
+	'Hamet'
+);
+
+INSERT INTO movies(
+	movie_names,
+	movie_times,
+	dimention 	
+)VALUES(
+	'the_shawshank_red',
+	'9_pm',
+	'3D'
+);
+
+INSERT INTO tickets(
+	seat_numbers,
+	room_info,
+	price
+)VAlUES(
+	'47',
+	'number_10',
+	'5.99'
+);
+
+INSERT INTO concessions(
+	popcorn,
+	reciept_emount,
+	drinks
+)VALUES(
+	'yes',
+	'9.47',
+	'Kambucha'
+);
